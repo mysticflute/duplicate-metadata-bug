67984a5a74472756f58e6624917ec2b48f423deb
Nathan McWilliams <mysticflute@users.noreply.github.com>
2024-03-19 14:02:32 -0400

    changes for bug report

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,21 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  description: "Description text for search engines.",
+
+  openGraph: {
+    type: "website",
+    url: "https://test.com",
+    title: "Open Graph Title",
+    description: "Description text for social media cards.",
+    images: "https://test.com/image.png",
+  },
+
+  twitter: {
+    card: "summary",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html>
